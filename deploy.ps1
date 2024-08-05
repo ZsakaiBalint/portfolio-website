85e32b9aa015f18c112bbe7d2bdfc03d1e3747d6
ZsakaiBalint <balintzsakai@gmail.com>
2024-08-05 17:42:47 +0200

    modified main.yml

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,23 @@
 param(
-    $ftpServer, 
-    $ftpUser, 
-    $ftpPass
+    [string]$ftpServer, 
+    [string]$ftpUser, 
+    [string]$ftpPass
 )
 
 $files = Get-ChildItem -Path '.' -File
 foreach ($file in $files) {
-    $ftpUri = 'ftp://'+$ftpServer+'/'+$file.Name
+    $ftpUri = "ftp://$ftpServer/$($file.Name)"
     $ftp = [System.Net.FtpWebRequest]::Create($ftpUri)
     $ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
     $ftp.Credentials = New-Object System.Net.NetworkCredential($ftpUser, $ftpPass)
 
-    $fileStream = [System.IO.File]::OpenRead($file.FullName)
-    $requestStream = $ftp.GetRequestStream()
-    $fileStream.CopyTo($requestStream)
-    $requestStream.Close()
-    $fileStream.Close()
+    try {
+        $fileStream = [System.IO.File]::OpenRead($file.FullName)
+        $requestStream = $ftp.GetRequestStream()
+        $fileStream.CopyTo($requestStream)
+        $requestStream.Close()
+        $fileStream.Close()
+    } catch {
+        Write-Error "Failed to upload file: $($file.Name). Error: $_"
+    }
 }
